40aa41db0fb9e63082d7efa6e557c350272f0aa8
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-11-26 13:26:43 -0700

    Basic React Setup

---
+++ canvas/webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var HtmlPlugin = require('html-webpack-plugin');
+var NotifierPlugin = require('webpack-notifier');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, "./build"),
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    modulesDirectories: ["node_modules", "./src"]
+  },
+  plugins: [
+    new HtmlPlugin({
+      title: 'Canvas Code Kata',
+      favicon: './src/favicon.png'
+    }),
+    new NotifierPlugin({
+      alwaysNotify: true
+    }),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: {baseDir: ['./build']}
+    })
+  ]
+};
